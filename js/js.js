0ac27cf5d3a1959b17223bc1e96a24a09c29ccf5
GiovanniR-S <giovannirobertssilva@outlook.com>
2021-01-26 19:28:04 -0300

    Responsivo

--- js/js.js
+++ js/js.js
@@ -1,6 +1,7 @@
 const tags = document.querySelectorAll('.tag');
 let selecionado = false;
 
+//Função para selecionar a tag
 function tagSelecinada(elemento){
 
     elemento.classList.add('select');
